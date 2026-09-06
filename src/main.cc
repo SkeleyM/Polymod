097d59d51d9a7509acbb781bed42644e6e688dac
SkeleyM <86805089+SkeleyM@users.noreply.github.com>
2026-09-06 21:59:34 +0100

    fix: selecting when dragging

--- src/main.cc
+++ src/main.cc
@@ -18,15 +18,30 @@
 #include <Ui/ToolbarTool/ExtrudeTool.h>
 #include <Ui/ToolbarTool/VertexSpinTool.h>
 
+#include <Renderer/LineRenderer.h>
+
 OrbitalCameraController* camera_controller;
 GeometryEditor geometry_editor;
 
 MenuBar menubar;
 
 static void on_render() {
-	static Vector2 mouse_pos{ 0.0f, 0.0f };
-	
 	InputManager& input = InputManager::get();
+	static Vector2 mouse_pos{ 0.0f, 0.0f };
+	static bool has_clicked = false;
+	static bool clicked_this_frame = false;
+
+	if (!has_clicked && input.get_mouse_buttons().first) {
+		has_clicked = true;
+		clicked_this_frame = true;
+	}
+	else if (has_clicked && input.get_mouse_buttons().first) {
+		clicked_this_frame = false;
+	}
+	else {
+		has_clicked = false;
+		clicked_this_frame = false;
+	}
 
 	// Calculate change in mouse position
 	Engine* engine = Engine::get_instance();
@@ -34,6 +49,8 @@ static void on_render() {
 
 	Vector2 mouse_delta = new_pos - mouse_pos;
 	mouse_pos = new_pos;
+
+	bool is_dragging = abs(mouse_delta.x + mouse_delta.y) > 10;
 
 	// If left clicking rotate using the change in mouse position.
 	if (engine->get_left_mouse() && !ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow | ImGuiHoveredFlags_AllowWhenBlockedByActiveItem))
@@ -47,7 +64,8 @@ static void on_render() {
 	menubar.render();
 
 	geometry_editor.render();
-	if (input.get_mouse_buttons().first) {
+	// Check that the mouse hasnt moved much this frame before we click
+	if (clicked_this_frame && !is_dragging) {
 		geometry_editor.select(input.get_mouse_pos());
 	}
 }
